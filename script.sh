9192ed754ea20ba9dbcb9839d912522166b727aa
AghayevEmin <emin.agayev.99@gmail.com>
2023-04-23 01:08:32 +0200

    added script

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ID="SD73990"
+
+DIRECTORY="/home/eminaghayev/Projects/STLA-testing"
+
+chmod 777 "/home/eminaghayev/Projects/STLA-testing/script.sh"
+
+if [ -d "$DIRECTORY/$ID" ]; then
+  echo "Folder with ID $ID already exists"
+else
+  echo "Folder with ID $ID does not exist, creating one..."
+  mkdir -m 777 "$DIRECTORY/$ID"
+  mkdir -m 777 "$DIRECTORY/$ID/Windows"
+  mkdir -m 777 "$DIRECTORY/$ID/Linux"
+fi
